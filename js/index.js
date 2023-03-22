c3a0c66c1be05a1317ab8d8d3ea7e898aa4fcd2a
KarenChuan <lanyonyon@gmail.com>
2023-03-23 04:07:11 +0800

    1.merge 檔案 2.新增fontawesome.scss檔案夾 3.移動normalize.scss到plugin資料夾 4.更新index.html(未完成) 5.新增刪除index資料夾圖片

--- js/index.js
+++ js/index.js
@@ -15,17 +15,39 @@
 // }).mount("#app");
 
 /**
- * canvas
+ * Render Header Footer
+ * @returns
  */
-//navbar下方水平線
-let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
-canvas.height = 2;
-let canvasWidth = window.innerWidth * 0.99;
-canvas.width = canvasWidth;
-ctx.lineWidth = 2;
-ctx.strokeStyle = "#ffffff";
-ctx.beginPath();
-ctx.moveTo(canvasWidth * 0.2, 1);
-ctx.lineTo(canvasWidth, 1);
-ctx.stroke();
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+includeHTML();
