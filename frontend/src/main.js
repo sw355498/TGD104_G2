7efed1de42347a4d7a0d330cc782cf313e0db752
sw355498 <sw355498@gmail.com>
2023-03-28 02:23:24 +0800

    引入assets/icon中所有的svg圖檔

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -16,4 +16,10 @@ import 'bootstrap'
 // js-grid套件引入
 import 'jsgrid/dist/jsgrid.min.css';
 import 'jsgrid/dist/jsgrid.min.js';
+
+//引入assets/img/
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+const req = require.context("@/assets/icon/", true, /\.svg$/)
+requireAll(req)
+
 createApp(App).use(router).mount('#app');
