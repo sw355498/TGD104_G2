6c24c37a82f97f182482a50ea6f4c3fdd09657d5
itshsinyu <itshsinyu@gmail.com>
2023-03-28 09:07:51 +0800

    修改p07_demoShopping vue、scss(放假瀏覽器)

--- frontend/src/views/p07_demoShopping.vue
+++ frontend/src/views/p07_demoShopping.vue
@@ -1,6 +1,32 @@
 <template>
-  <div class="home">
+  <div class="main_p07_demoShopping">
+    <!-- 瀏覽器 -->
+    <section class="browser_p07_demoShopping">
+        <div class="browser_top_demoShopping">
+            <div class="toolbar_top_demoShopping">
+                <div class="circles_demoShopping"></div>
+                <div class="tab_demoShopping">
+                    <i class="fa-solid fa-globe"></i>
+                    <span>詐騙網站</span>
+                </div>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div class="toolbar_down_demoShopping">
+                <i class="fa-solid fa-arrow-left"></i>
+                <i class="fa-solid fa-arrow-right"></i>
+                <i class="fa-solid fa-rotate-right"></i>
+                <div class="address_demoShopping">
+                    <i class="fa-solid fa-lock"></i>
+                    <span>http://shopee.rip/te9</span>
+                </div>
+                <i class="fa-solid fa-ellipsis-vertical"></i>
+            </div>
+        </div>
 
-    <h1>Welcome to Your Vue.js App</h1>
+        <article class="scamWeb_p07_demoShopping_container">
+            
+
+        </article>
+    </section>
   </div>
 </template>
