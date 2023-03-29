12d3c03656261bfc9b7a46562d7aa52a4c827ef3
sw355498 <sw355498@gmail.com>
2023-03-30 02:02:59 +0800

    套件安裝後測試

--- frontend/src/views/p06_discuss.vue
+++ frontend/src/views/p06_discuss.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="home">
-
-    <h1>Welcome to Your Vue.js App</h1>
-  </div>
+    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
 </template>
+
+<script>
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                editor: ClassicEditor,
+                editorData: '<p>Content of the editor.</p>',
+                editorConfig: {
+                    // The configuration of the editor.
+                }
+            };
+        }
+    }
+</script>
