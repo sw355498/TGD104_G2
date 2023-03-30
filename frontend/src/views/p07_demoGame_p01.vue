c3c9a9e078d4e0ba145f4bfcd9773b1e9d066440
candybomb <shigotolucky@gmail.com>
2023-03-30 08:58:13 +0800

    修改section 增加class

--- frontend/src/views/p07_demoGame_p01.vue
+++ frontend/src/views/p07_demoGame_p01.vue
@@ -1,6 +1,6 @@
 <template>
  
-    <section>
+    <section class="p07_demoGame_p01bg">
 
         <div class="quiz_background">
 
@@ -13,7 +13,7 @@
 
             <div class="p07_demoGame_01_quiz_view">
                 <div class="p07_demoGame_01_quiz_question_title">
-                    <p>6.在疫情期間，政府為體恤人民困苦，有發放補助金之行為，然而有不肖詐騙集團以政府發放補助金為名義，利用簡訊方式詐騙人民個資，請問哪一個網址才是正確連結政府網站的網址？</p>
+                    <p>在疫情期間，政府為體恤人民困苦，有發放補助金之行為，然而有不肖詐騙集團以政府發放補助金為名義，利用簡訊方式詐騙人民個資，請問哪一個網址才是正確連結政府網站的網址？</p>
                 </div>
                 <img src="../assets/img/p07_demo/p07_demoGame/quiz1.jpg" alt="">
             </div>
@@ -27,9 +27,9 @@
 
             <!-- 印章部分 -->
 
-            <div class="seal">
+            <!-- <div class="seal">
                 <p>正確</p>
-            </div>
+            </div> -->
 
 
             </div>
@@ -38,7 +38,7 @@
             <!-- 解答部分 -->
 
 
-            <div class="answer_zone">
+            <!-- <div class="answer_zone">
                 <div class="answer_board">
                     <h2>破解</h2>
                     <p>網路交友陷阱多，尤其一旦陷入戀愛更容易沖昏頭，網戀注意以下幾點，讓你遠離詐騙爛桃花<br>1.永遠不要把感情與錢放在一起，真要放一起，就要當作這筆錢已經丟了<br>2.認識一周以內，就會叫你老公、老婆的人，絕對不能相信<br>3.認識一周以後，叫妳老公、老婆的人，談到投資，只能他給你錢，絕對不要給他錢<br>4.伴侶或朋友有錢，不關你的事，祝福他就好，請不要投資他<br>5.年投資報酬率超過20%，風險可能超過200%。低風險、高報酬，得想想為什麼會是自己<br>6.匯款的時候，請跟銀行行員老實交代原因，他們都受過訓練，會幫你分辨真假，但是如果你騙他們，那就愛莫能助了<br>7.錢不要匯到國外！那會追不回來，連人頭帳戶都無法追查</p>
@@ -46,10 +46,10 @@
                     </div> 
 
 
-            </div>
+            </div> -->
 
 
-            <div class="next">
+            <!-- <div class="next">
 
                 <button class="next_quiz_botton">
                    <p class="botton_p">
@@ -64,7 +64,7 @@
 
                 <div class="next_quiz_frame"></div>
                 
-            </div>
+            </div> -->
             
 
 
