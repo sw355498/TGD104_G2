25f34a27cc88484fc7f6f7d90e412d61b4a3ef40
sw355498 <sw355498@gmail.com>
2023-03-30 20:45:52 +0800

    jsGrid暫存

--- frontend/src/_App.vue
+++ frontend/src/_App.vue
@@ -15,14 +15,15 @@
     </div>
 </template>
 <script>
-import $ from 'jquery'
 import 'jsgrid'
-import LeftNav from './components/b_leftNav.vue';
+import LeftNav from '../components/b_leftNav.vue';
 export default {
-    
-    mounted() {
+    components:{
+        LeftNav
+    },
+    steup(){
         let id = 0, user = 111
-        var clients = [
+        const clients = [
             { "ID": `${id++}`, "帳號": `user_${user}`, "暱稱": "Otto Clay", "建立日期": '2023-03-19', "E-mail": `user_${user++}@test.com`, "登入方式": "會員註冊", "狀態": "正常", },
             { "ID": `${id++}`, "帳號": `user_${user}`, "暱稱": "Otto Clay", "建立日期": '2023-03-19', "E-mail": `user_${user++}@test.com`, "登入方式": "Google", "狀態": "正常", },
             { "ID": `${id++}`, "帳號": `user_${user}`, "暱稱": "Otto Clay", "建立日期": '2023-03-19', "E-mail": `user_${user++}@test.com`, "登入方式": "會員註冊", "狀態": "正常", },
@@ -53,7 +54,6 @@ export default {
             inserting: false, //添加
             editing: false, //編輯
             sorting: true, //排序
-
             /*分頁設定*/
             paging: true,
             pagerContainer: null,   //jQueryElement或DomNode指定呈現一個分頁欄，為null時在表格底部。
@@ -67,7 +67,6 @@ export default {
             pageLastText: "最後一頁",   //尾頁
             pageNavigatorNextText: "...",    //最大數量頁面按鈕超出時右邊顯示
             pageNavigatorPrevText: "...",       //最大數量頁面按鈕超出時右邊顯示
-
                         data: clients,
             fields: [
                 { 
@@ -108,23 +107,18 @@ export default {
                 {
                     name: "操作",
                     width: 150,
-                    itemTemplate:function(){
+                    itemTemplate:() => {
                         let $buttonContainer = $("<div>")
-                        // Jquery 等同於 js中的 let buttonContainer = document.createElement("<div>")
-
-                        // let $blockade = $("<button>").html(`<i class="fa-solid fa-lock"></i>封鎖`).addClass("small_button mx-1").on("click", operate);
+ 
                         let $blockade = $("<button>").text(`封鎖`).addClass("small_button mx-1").on("click", operate);
                         $buttonContainer.append($blockade);
-
                         let $delete = $("<button>").text("刪除").addClass("small_button mx-1").on("click", operate);
                         $buttonContainer.append($delete);
-
                         let $revise = $("<button>").text("修改").addClass("small_button mx-1").on("click", operate);
                         $buttonContainer.append($revise);
                         
                         let $check = $("<button>").text("查看").addClass("small_button mx-1").on("click", operate);
                         $buttonContainer.append($check);
-
                         return $buttonContainer
                     },
                 },
@@ -149,11 +143,9 @@ export default {
             }
         }
     },
-    components: {
-        LeftNav
-    }
 }
 </script>
+
 <style>
         .fa-sharp{
             transform: translate(-50%, -50%);
