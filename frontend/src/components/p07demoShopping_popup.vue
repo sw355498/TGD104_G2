6815d52c0585c918c5aec90ccda1a165c5a8af70
bbn718 <vanishing0718@gmail.com>
2023-04-01 16:31:51 +0800

    只是先commit一下保存進度

---
+++ frontend/src/components/p07demoShopping_popup.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+        <div class="popup_p07_demoShopping_arrow">
+
+        </div>
+        <div class="popup_p07_demoShopping_container">
+            <div class="popup_p07_demoShopping_textArea">
+                <i class="fa-solid fa-xmark"></i>
+                <h3>詐騙網站特徵</h3>
+                <p>1. 檢查網址 (拼音奇特、與正版網址差幾個字)</p>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script></script>
+
+<style scoped></style>
+
--- frontend/src/views/p07_demoLine.vue
+++ frontend/src/views/p07_demoLine.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="body_p07_demoLine">
+
     <!-- demo 體驗的 nav -->
     <p07_demo_nav />
     <section class="main_p07_demoLine">
+
       <!-- line 背景 -->
       <div class="line_p07_demoLine">
+
+        <!-- line 聊天區 -->
         <div class="line_p07_demoLine_chatContainer">
-          
+
+          <!-- line 聊天框框 -->
+          <div class="line_p07_demoLine_chatMessage">
+            <img src="../assets/img/p07_demo/p07_demoLine/a_user_pic.png">
+            <div class="line_p07_demoLine_chatMessages">
+              <p class="line_p07_demoLine_chatMessagesName">Matthew Wu</p>
+              <div class="line_p07_demoLine_chatMessagesContent">
+                <p class="line_p07_demoLine_chatMessagesContents">抱歉~打擾您一下</p>
+              </div>
+            </div>
+            <span class="line_p07_demoLine_chatMessageTimestamp"></span>
+          </div>
         </div>
         <!-- <img src="../assets/img/p07_demo/p07_demoLine/linebg.png" alt="Line"> -->
       </div>
       <!-- 筆 -->
       <div class="applepencil_p07_demoLine">
-        <img src="../assets/img/p07_demo/p07_demoLine/ApplePencil.png" alt="applepencil">
+        <img src="../assets/img/p07_demo/p07_demoLine/a_user_pic.png" alt="applepencil">
       </div>
     </section>
   </div>
