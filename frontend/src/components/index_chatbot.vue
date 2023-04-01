df1834b41c355b7e5b6af02989eb05f0cf8c56ec
itshsinyu <itshsinyu@gmail.com>
2023-04-01 23:03:51 +0800

    修改index機器人scss跟一點程式

--- frontend/src/components/index_chatbot.vue
+++ frontend/src/components/index_chatbot.vue
@@ -3,10 +3,10 @@
     <!-- <div class="chatbot_index" v-if="show" > -->
     <div class="chatbot_index" >
         <div class="chatTitle">
-            <figure class="robot">
-                <!-- <img src="../assets/icon/robot.svg" /> -->
+            <img src="../assets/img/icon/robot.png" />
+            <!-- <figure class="robot">
                 <svg><use xlink:href="#robot" /></svg>
-            </figure>
+            </figure> -->
             <div class="chatTitle_text">
                 <p>專屬機器人</p>
                 <p>詐知就好</p>
@@ -16,26 +16,44 @@
         <div class="chatWindow">
             <!-- 一開始就有的文字 -->
             <div class="chatWindow_message_default">
-                <div class="chat__bot-message_default">
-                    <span>你好!</span>
-                    <span>這些資訊我想你會需要</span>
+                <div class="chatbot_message_default">
+                    <p>你好！</p>
                 </div>
-                <div class="chat__bot-news">
-
+                <div class="chatbot_message_default">
+                    <p>趕快來看看有哪些最新消息吧</p>
+                </div>
+                <div class="chatbot_news_container">
+                    <a class="chatbot_news_item" href="">
+                        <span>最新消息</span>
+                        <p>請注意近期詐騙集團假冒CACO客服解除分期付款詐騙</p>
+                    </a>
+                    <a class="chatbot_news_item" href="">
+                        <span>最新消息</span>
+                        <p>假冒銀行信用貸款簡訊</p>
+                    </a>
+                    <a class="chatbot_news_item" href="">
+                        <span>最新消息</span>
+                        <p>請注意近期詐騙集團假冒「民宿、旅店、露營區」客服解除分期付款詐騙</p>
+                    </a>
+                    <a class="chatbot_news_item" href="">
+                        <span>最新消息</span>
+                        <p>假冒銀行信用貸款簡訊</p>
+                    </a>
                 </div>
             </div>
             <div class="chatWindow_message" v-for="(message, index) in messages" :key="index">
-                <div v-if="message.isBot" class="chat__bot-message">
-                    <p>{{ message.text }}</p>
+                <div v-if="message.isBot" class="bot_message">
+                    <p v-html="message.text"></p>
+                    <!-- <p>{{ message.text }}</p> -->
                 </div>
-                <div v-else class="chat__user-message">
+                <div v-else class="user_message">
                     <p>{{ message.text }}</p>
                 </div>
             </div>
         </div>
         <div class="chatInput">
             <input type="text" class="message-input" v-model="inputValue" @keydown.enter="sendMessage" placeholder="請輸入訊息...">
-            <button type="submit" class="message-submit chat__send-button"  @click="sendMessage">➤</button>
+            <button type="submit" class="message-submit"  @click="sendMessage">➤</button>
         </div>
     </div>
     <!-- <svg style="position:fixed;bottom: 5%; right: 1%;"><use xlink:href="#robot" @click="showComponent = true; listenClickEvent"/></svg> -->
@@ -46,34 +64,37 @@
 
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref } from 'vue'
 export default {
   name: 'Chat',
   setup() {
     // console.log(props.show);
     const messages = ref([]) // 存儲聊天記錄的數組
     const inputValue = ref('') // 存儲輸入框的值
+    const goodbyeTimer = ref(null) // 存儲setTimeout返回的計時器ID
     function sendMessage() {
-      if (inputValue.value) {
-        // 將用戶輸入的訊息加入到聊天記錄數組中
-        messages.value.push({
-          text: inputValue.value,
-          isBot: false,
-        })
-        // 獲取聊天機器人的回覆
-        const botMessage = getBotMessage(inputValue.value)
-        // 將聊天機器人的回覆加入到聊天記錄數組中
-        messages.value.push({
-          text: botMessage,
-          isBot: true,
-        })
-        // 清空輸入框
-        inputValue.value = ''
-      }
+        // 取消計時器
+        clearTimeout(goodbyeTimer.value)
+        if (inputValue.value) {
+            // 將用戶輸入的訊息加入到聊天記錄數組中
+            messages.value.push({
+                text: inputValue.value,
+                isBot: false,
+            })
+            // 獲取聊天機器人的回覆
+            const botMessage = getBotMessage(inputValue.value)
+            // 將聊天機器人的回覆加入到聊天記錄數組中
+            messages.value.push({
+                text: botMessage,
+                isBot: true,
+            })
+            // 清空輸入框
+            inputValue.value = ''
+        }
     }
     function getBotMessage(input) {
         // 根據用戶的輸入獲取聊天機器人的回覆
-        let botMsg = '';
+        let botMsg = ``;
         switch (input.toLowerCase()) {
             case 'hi':
             case 'hello':
@@ -81,27 +102,67 @@ export default {
             botMsg = 'Hello there!';
             break;
             case '防範詐騙教學':
-            botMsg = '防範詐騙教學!';
+            botMsg =  `以下是防範詐騙教學的相關資訊<br>
+                <a href="#" style="color:black">
+                    點我看更多
+                </a>
+            `;
             break;
             case '回報可疑網站':
-            botMsg = '回報可疑網站!';
+            botMsg = `以下是回報可疑網站的相關資訊<br>
+                <a href="#" style="color:black">
+                    點我看更多
+                </a>
+            `;
             break;
             case '詐騙FAQ':
-            botMsg = 'M詐騙FAQ';
+            botMsg = `以下是詐騙FAQ的相關資訊<br>
+                <a href="#" style="color:black">
+                    點我看更多
+                </a>
+            `;
             break;
             case '政府資訊連結':
-            botMsg = 'G政府資訊連結!';
+            botMsg = `以下是政府資訊連結的相關資訊<br>
+                <a href="#" style="color:black">
+                    點我看更多
+                </a>
+            `;
             break;
             case '討論專區':
-            botMsg = '討論專區';
+            botMsg = `以下是討論專區的相關資訊<br>
+                <a href="#" style="color:black">
+                    點我看更多
+                </a>
+            `;
             break;
             default:
-            botMsg = 'Sorry, I did not understand what you said.';
+            botMsg = `我猜你應該想知道<br>
+            1.防範詐騙教學<br>
+            2.回報可疑網站<br>
+            3.詐騙FAQ<br>
+            4.政府相關資訊連結<br>
+            5.討論專區<br>
+            6.DEMO體驗<br>
+            `;
         }
+        // 啟動計時器，30秒後顯示再見的消息
+        goodbyeTimer.value = setTimeout(() => {
+            messages.value.push({
+                text: `已經超過30秒沒有輸入文字囉，要不要看看下面的內容啊<br>
+                1.防範詐騙教學<br>
+                2.回報可疑網站<br>
+                3.詐騙FAQ<br>
+                4.政府相關資訊連結<br>
+                5.討論專區<br>
+                6.DEMO體驗<br>
+            `,
+                isBot: true,
+            })
+        }, 3000)
         return botMsg;
     }
     
-
     return {
       messages,
       inputValue,
@@ -110,158 +171,3 @@ export default {
   },
 }
 </script>
-<style scoped lang="scss">
-
-.clost-bg{
-//   position: absolute;
-  position: fixed;
-//   z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background:rgba(0,0,0,0.5);
-}
-.chatbot_index {
-    position: fixed;
-    top: 50%;
-    right: 20%;
-    transform: translate(20%, -50%);
-    max-width: 700px;
-    width: 100%;
-    height: 600px;
-    z-index: 2;
-    overflow: hidden;
-    background: #EBEBEB;
-    border-radius: 20px;
-    display: flex;
-    justify-content: space-between;
-    flex-direction: column;
-    box-shadow: 0px 0px 5px black;
-    // 上面 chatTitle
-    .chatTitle {
-        flex: 0 1 45px;
-        z-index: 2;
-        background: white;
-        text-align: left;
-        display: flex;
-        // height: 90px;
-        .robot svg {
-            width: 100%;
-            // width: 90px;
-            // height: 90px;
-            object-fit: cover;
-        }
-        .chatTitle_text{
-            margin: 20px 0;
-            p{
-            font-size: 20px;
-            font-weight: 500;
-            margin: 10px;
-            color: black;
-            }
-            p:last-child {
-                font-size: 18px;
-                font-weight: 300;
-                letter-spacing: 1px;
-            }
-        }
-        .fa-xmark{
-            position: absolute;
-            right: 20px;
-            top: 20px;
-            width: 50px;
-            height: 50px;
-        }
-    }
-    // ============ 對話 ============
-    .chatWindow {
-        flex: 1 1 auto;
-        color: rgba(255, 255, 255, 0.5);
-        // overflow: hidden;
-        overflow-y: scroll;
-        // position: relative;
-        width: 100%;
-        .chatWindow_message_default{
-            margin: 10px 10px 0;
-            border-radius: 10px;
-            color: #033159;
-            display: flow-root;
-            .chat__bot-message_default span{
-                outline: 1px solid red;
-                background-color: white;
-                padding: 10px;
-                border-radius: 5px;
-                max-width: 40%;
-                width: auto;
-                word-break: break-word;
-                color: #333;
-                // float: left;
-            }
-        }
-        .chatWindow_message{
-            margin: 0 10px;
-            border-radius: 10px;
-            // color: $sec_color;
-            color: #033159;
-            display: flow-root;
-            .chat__bot-message p,
-            .chat__user-message p {
-                padding: 10px;
-                border-radius: 5px;
-                max-width: 80%;
-                word-break: break-word;
-            }
-            .chat__bot-message p {
-                background-color: white;
-                color: #333;
-                float: left;
-            }
-            .chat__user-message p {
-                // background: $sec_color;
-                background-color: #033159;
-                color: white;
-                float: right;
-
-            }
-        }
-    }
-    // ============ 使用者輸入的地方 ============
-    .chatInput {
-        flex: 0 1 40px;
-        // width: 100%;
-        background: white;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        .message-input {
-            background: none;
-            border: none;
-            outline: none !important;
-            resize: none;
-            color: rgba(0, 0, 0, 0.7);
-            padding-left: 20px;
-            width: 100%;
-            font-size: 16px;
-            height: 100%;
-        }
-        .message-submit {
-            color: #fff;
-            border: none;
-            background-color: #0fb2f2;
-            // background-color: $four_color;
-            font-size: 16px;
-            padding: 6px 10px;
-            margin: 10px;
-            border-radius: 10px;
-            outline: none !important;
-            &:hover{
-                // background: $sec_color;
-                background: #033159;
-                cursor: pointer;
-            }
-        }
-    }
-}
-
-</style> 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,7 +6,9 @@ const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 
 module.exports = defineConfig({
     transpileDependencies: [/ckeditor5-[^/\\]+[/\\]src[/\\].+\.js$/],
-    publicPath:'./',
+    publicPath: process.env.NODE_ENV === 'production'
+		? '/tgd104/g2/'
+		: '/',
     configureWebpack: {
         plugins: [
             //jQuery 設定
