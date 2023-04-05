614670d51869e6ffadd1df6838cc65827a804837
sw355498 <sw355498@gmail.com>
2023-04-05 15:14:13 +0800

    討論專區列表rwd

--- frontend/src/views/p06_discuss.vue
+++ frontend/src/views/p06_discuss.vue
@@ -1,89 +1,108 @@
 <template>
-    <main class="wrapper_p06_discuss">
-        <ul class="breadcrumb-list text_title">
-            <li><router-link to="/index">首頁</router-link></li>
-            <li>討論專區</li>
-        </ul>
-        <div class="titleBlock">
-            <h2>所有文章</h2>
-            <button class="medium_button">發表新文章</button>
-        </div>
-        <div class="tab_p01_newsChoose">
-            <div class="tab_p01_news_category">
-                <div class="wide_tab">
-                    <a href="#" :class="{ currentTab: isActive }">所有文章</a>　|　<a
-                    href="#"
-                    >網站詐騙</a
-                    >　|　<a href="#" >交友詐騙</a>　|　<a href="#">金融詐騙</a>
-                </div>
-            </div>
-            <div class="input_p01_news_searchBar">
-            <input type="text" placeholder="搜尋文章" />
-            <div class="icon">
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </div>
-            </div>
-        </div>
-        <section v-for="item in articleList" :kye="item.id">
-                <div class="topBlock_p06_discuss">
-                    <div class="author">
-                        <img src="../assets/img/p08_user/user.jpg" alt="cat"  class="pic_p06_discuss"/>
-                        <span class="paragraph">{{item.author}}</span>
-                    </div>
-                    <button class="ellipsisBtn" @click="ellipsisBtn"><i class="fa-solid fa-ellipsis-vertical"></i></button>
-                </div>
-                <div class="articleBlock_p06_discuss">
-                    <div class="articleText_p06_discuss">
-                        <h3>{{item.title}}</h3>
-                        <div class="articleInformation_p06_discuss text_title">
-                            <span class="tag">#{{item.tag}}</span>
-                            <span>・</span>
-                            <span>{{item.time}}</span>
-                        </div>
-                        <div class="articleContent h6_component">
-                            {{item.content}}
-                        </div>
-                        <ul class="articleInteractive text_title">
-                            <li><i class="fa-solid fa-thumbs-up fa-fw"></i><span> {{item.thumbsNum}} </span></li>   
-                            <li><i class="fa-solid fa-message fa-fw"></i><span> {{item.messageNum}} </span></li>
-                            <!-- <li><i class="fa-solid fa-bookmark fa-fw"></i><span> 收藏 </span></li> -->
-                            <li>
-                                <router-link to="/discuss/discuss_content">
-                                    <button class="viewDetails medium_button">
-                                        查看詳細
-                                    </button>
-                                </router-link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="articleImage_p06_discuss">
-                        <img src="https://picsum.photos/200/300" alt="">
-                    </div>
-                </div>
-        </section>
-        <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center">
-                <li class="page-item">
-                <a class="page-link pagination-dark" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-                </li>
-                <li class="page-item"><a class="page-link  pagination-dark" href="#">1</a></li>
-                <li class="page-item"><a class="page-link  pagination-dark" href="#">2</a></li>
-                <li class="page-item"><a class="page-link  pagination-dark" href="#">3</a></li>
-                <li class="page-item">
-                <a class="page-link pagination-dark" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-                </li>
+    <div>
+        <!-- navgation -->
+        <frontNavbar />
+        <main class="wrapper_p06_discuss">
+            <ul class="breadcrumb-list text_title">
+                <li><router-link to="/index">首頁</router-link></li>
+                <li>討論專區</li>
             </ul>
-        </nav>
-    </main>
+            <div class="titleBlock">
+                <h2>所有文章</h2>
+                <button class="medium_button"><router-link to="/discuss_new_content">發表新文章</router-link></button>
+            </div>
+            <div class="tab_p01_newsChoose">
+                <div class="tab_p01_news_category">
+                    <select name="" id="" class="form-select form-select-md">
+                        <option selected>所有文章</option>
+                        <option value="">網站詐騙</option>
+                        <option value="">交友詐騙</option>
+                        <option value="">金融詐騙</option>
+                    </select>
+                    <div class="wide_tab">
+                        <a href="#" :class="{ currentTab: isActive }">所有文章</a>　|　<a
+                        href="#"
+                        >網站詐騙</a
+                        >　|　<a href="#" >交友詐騙</a>　|　<a href="#">金融詐騙</a>
+                    </div>
+                </div>
+                <div class="input_p01_news_searchBar">
+                    <input type="text" placeholder="搜尋文章" />
+                    <div class="icon">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                </div>
+            </div>
+            <section v-for="item in articleList" :kye="item.id">
+                    <div class="topBlock_p06_discuss">
+                        <div class="author">
+                            <img src="../assets/img/p08_user/user.jpg" alt="cat"  class="pic_p06_discuss"/>
+                            <span class="paragraph">{{item.author}}</span>
+                        </div>
+                        <button class="ellipsisBtn" @click="ellipsisBtn"><i class="fa-solid fa-ellipsis-vertical"></i></button>
+                    </div>
+                    <div class="articleBlock_p06_discuss">
+                        <div class="articleText_p06_discuss">
+                            <h3>{{item.title}}</h3>
+                            <div class="articleInformation_p06_discuss text_title">
+                                <span class="tag">#{{item.tag}}</span>
+                                <span>・</span>
+                                <span>{{item.time}}</span>
+                            </div>
+                            <div class="articleContent h6_component">
+                                {{item.content}}
+                            </div>
+                            <ul class="articleInteractive text_title">
+                                <li><i class="fa-solid fa-thumbs-up fa-fw"></i><span> {{item.thumbsNum}} </span></li>   
+                                <li><i class="fa-solid fa-message fa-fw"></i><span> {{item.messageNum}} </span></li>
+                                <!-- <li><i class="fa-solid fa-bookmark fa-fw"></i><span> 收藏 </span></li> -->
+                                <li>
+                                    <router-link to="/discuss/discuss_content">
+                                        <button class="viewDetails medium_button">
+                                            查看詳細
+                                        </button>
+                                    </router-link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="articleImage_p06_discuss">
+                            <img src="https://picsum.photos/200/300" alt="">
+                        </div>
+                    </div>
+            </section>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item">
+                    <a class="page-link pagination-dark" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                    </li>
+                    <li class="page-item"><a class="page-link  pagination-dark" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link  pagination-dark" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link  pagination-dark" href="#">3</a></li>
+                    <li class="page-item">
+                    <a class="page-link pagination-dark" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                    </li>
+                </ul>
+            </nav>
+        </main>
+        <!-- footer -->
+        <frontFooter />
+    </div>
 </template>
 
 <script>
-    import { ref } from 'vue'
+    import { ref } from 'vue';
+    import frontNavbar from "@/components/f_nav.vue";
+    import frontFooter from "@/components/f_footer.vue";
+
     export default {
+        components: {
+            frontNavbar,
+            frontFooter,
+        },
         setup(){
             let id = 0
             const articleList = ref([
@@ -97,6 +116,8 @@
             return{
                 isActive: true,
                 articleList,
+                frontNavbar,
+                frontFooter,
             }
         }
     }
