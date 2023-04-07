0fa8e316a35f75029797b353f64b56f37d53d4ec
KarenChuan <lanyonyon@gmail.com>
2023-04-07 16:58:50 +0800

    1.登入畫面新增Title

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,70 +1,87 @@
 <template>
-
-      
-      <nav v-if="$route.path === '/'">
-        <div class="router_block">      
-          <router-link to="/index" class="router_item">
-            <div>
-              進入前台
-            </div>
-          </router-link>
-          <router-link to="/b_index" class="router_item">
-            <div>
-              進入後台
-            </div>
-          </router-link>
-        </div>
-      </nav>
-      <router-view></router-view>
-      <div id="particles-js" v-if="$route.path === '/'"></div>
+  <nav v-if="$route.path === '/'">
+    <div class="title">詐知就好</div>
+    <div class="router_block">
+      <router-link to="/index" class="router_item">
+        <div>進入前台</div>
+      </router-link>
+      <router-link to="/b_index" class="router_item">
+        <div>進入後台</div>
+      </router-link>
+    </div>
+  </nav>
+  <router-view></router-view>
+  <div id="particles-js" v-if="$route.path === '/'"></div>
 </template>
 <script>
 import particlesJs from "particles.js";
 import particlesConfig from "@/assets/particles.json";
 export default {
   data() {
-      return {};
+    return {};
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      particlesJS("particles-js", particlesConfig);
+      document.body.style.overflow = "hidden";
     },
-    mounted() {
-      this.init();
-    },
-    methods: {
-      init() {
-        particlesJS("particles-js", particlesConfig);
-        document.body.style.overflow = "hidden";
-      }
-    }
+  },
 };
 </script>
 <style lang="scss">
-  #particles-js{
-    width: 100vw;
-    height: 100vh;
-  }
-  .router_block{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    column-gap: 25px;
-    row-gap: 25px;
-    flex-wrap: wrap;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  .router_item{
-    padding: 80px;
-    background: rgba($color: #13e5f2, $alpha: 0.5);
-    color: #fff;
-    font-size: 2rem;
+#particles-js {
+  width: 100vw;
+  height: 100vh;
+}
+.router_block {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 25px;
+  row-gap: 25px;
+  flex-wrap: wrap;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.router_item {
+  padding: 80px;
+  background: rgba($color: #13e5f2, $alpha: 0.5);
+  color: #fff;
+  font-size: 2rem;
 
-    &hover{
-      background: rgba($color: #13e5f2, $alpha: 1);
-    }
-    div{
-      width: 130px;
-      text-align: center;
-    }
+  &hover {
+    background: rgba($color: #13e5f2, $alpha: 1);
   }
+  div {
+    width: 130px;
+    text-align: center;
+  }
+}
+
+.title {
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 3rem;
+  letter-spacing: 0.5rem;
+  color: #fff;
+  animation: neon2 1.5s ease-in-out infinite alternate;
+  // padding: 50px;
+}
+@keyframes neon2 {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #228dff,
+      0 0 70px #228dff, 0 0 80px #228dff, 0 0 100px #228dff, 0 0 150px #228dff;
+  }
+  to {
+    text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #228dff,
+      0 0 35px #228dff, 0 0 40px #228dff, 0 0 50px #228dff, 0 0 75px #228dff;
+  }
+}
 </style>
