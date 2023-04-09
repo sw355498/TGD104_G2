550d77eb9b1b8c2ef34cd45021b81e0fd4893686
itshsinyu <itshsinyu@gmail.com>
2023-04-09 17:54:57 +0800

    修改 conn.php 加 gitignore

--- frontend/src/api/getConn.php
+++ frontend/src/api/getConn.php
@@ -13,21 +13,10 @@
 
 //---------------------------------------------------
 
-// 建立一個 Express application 物件
+    // 建立一個 Express application 物件
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: GET, POST");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-//建立SQL語法
-$sql = "SELECT * FROM member";
-
-//執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
-$statement = $pdo->query($sql);
-
-$data = $statement->fetchAll();
-
-echo json_encode($data);
-       
 
 ?>
