c2ecc4f3abf1d1705fec4e250f637a4987164e54
sw355498 <sw355498@gmail.com>
2023-04-09 11:28:34 +0800

    彈窗components

---
+++ frontend/src/components/modal.vue
@@ -0,0 +1,33 @@
+<template>
+  <Transition name="modal">
+    <div v-if="show" class="modal-wrapper">
+      <div class="modal-container">
+        <div class="modal-header">
+          <slot name="header">default header</slot>
+          <button
+              class="modal-default-button"
+              @click="$emit('close')"
+          >Ｘ</button>
+        </div>
+
+        <div class="modal-body">
+            
+        </div>
+
+        <div class="modal-footer">
+            <button
+              class="modal-default-button"
+              @click="$emit('close')"
+          >送出</button>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+    const props = defineProps({
+        show: Boolean
+    })
+</script>
+
