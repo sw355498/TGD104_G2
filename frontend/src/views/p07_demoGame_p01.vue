a750b88521569bfaa3f334e647167aea228ca24c
candybomb <shigotolucky@gmail.com>
2023-04-11 18:39:02 +0800

    新增發光特效

--- frontend/src/views/p07_demoGame_p01.vue
+++ frontend/src/views/p07_demoGame_p01.vue
@@ -1,45 +1,41 @@
 <template>
  
-    <section class="p07_demoGame_p01bg">
+    <section id="app" class="p07_demoGame_p01bg">
 
         <div class="quiz_background">
 
             <div class="p07_demoGame_01_question_number">
                 <div class="quiz_number_frame">
-                    <h1 class="quiz_question_num">第一題</h1>
+                    <h1 class="quiz_question_num">第{{numberToChinese(questionNumber)}} 題</h1>
                 </div>
                 
             </div>
 
             <div class="p07_demoGame_01_quiz_view">
                 <div class="p07_demoGame_01_quiz_question_title">
-                    <p>在疫情期間，政府為體恤人民困苦，有發放補助金之行在疫情期間，政府為體恤人民困苦，有發放補助金之行在疫情期間，政府為體恤人民困苦，有發放補助金之行在疫情期間，政府為體恤人民困苦，有發放補助金之行</p>
+                    <p>{{questionText}}</p>
                 </div>
-                <img src="../assets/img/p07_demo/p07_demoGame/quiz1.jpg" alt="">
+                <img :src="questionImage"  alt="">
             </div>
             
             <div class="answer_button">
-                <div class="answer answer_button_01">
-                    
-                   
+                <div class="answer answer_button_01" >
                    <p> C.領了之後拆開發現裡面根本不是自己當初買的東西，大叫倒楣被騙</p> </div>
                 <div class="answer answer_button_02">
-                    <!-- <img src="../assets/img/p07_demo/p07_demoGame/question_botton.png" alt=""> -->
                     <p>C.領了之後拆開發現裡面根本不是自己當初買的東西，大叫倒楣被騙</p> </div>
                 <div class="answer answer_button_03">
-                    <!-- <img src="../assets/img/p07_demo/p07_demoGame/question_botton.png" alt=""> -->
                     <p>A.對方的大頭貼放著帥哥美女圖，而且對方跟你網戀一段時間後就用各種名目向你要錢</p> 
                     </div>
                 <div class="answer answer_button_04">
-                    <!-- <img src="../assets/img/p07_demo/p07_demoGame/question_botton.png" alt=""> -->
+
                     <p>D.他不會情緒勒索你，強迫你交一些隱私資料，如信用卡卡號，銀行帳戶，身分證號碼等</p> 
                 </div>
 
             <!-- 印章部分 -->
-
+<!-- 
             <div class="seal">
                 <p>正確</p>
-            </div>
+            </div> -->
 
 
             </div>
@@ -48,7 +44,7 @@
             <!-- 解答部分 -->
 
 
-            <div class="answer_zone">
+            <!-- <div class="answer_zone">
                 <div class="answer_board">
                     <h2>破解</h2>
                     <p>網路交友陷阱多，尤其一旦陷入戀愛更容易沖昏頭，網戀注意以下幾點，讓你遠離詐騙爛桃花<br>1.永遠不要把感情與錢放在一起，真要放一起，就要當作這筆錢已經丟了<br>2.認識一周以內，就會叫你老公、老婆的人，絕對不能相信<br>3.認識一周以後，叫妳老公、老婆的人，談到投資，只能他給你錢，絕對不要給他錢<br>4.伴侶或朋友有錢，不關你的事，祝福他就好，請不要投資他<br>5.年投資報酬率超過20%，風險可能超過200%。低風險、高報酬，得想想為什麼會是自己<br>6.匯款的時候，請跟銀行行員老實交代原因，他們都受過訓練，會幫你分辨真假，但是如果你騙他們，那就愛莫能助了<br>7.錢不要匯到國外！那會追不回來，連人頭帳戶都無法追查</p>
@@ -56,20 +52,20 @@
                     </div> 
 
 
-            </div>
+            </div> -->
 
 
             <div class="next">
 
-                <button class="next_quiz_botton">
-                   <router-link to='/p07demoGameresult' class="botton_p">
+                <button class="next_quiz_botton"  @click="changeQuestionAndScrollToTop">
+                   <div class="botton_p">
                     下一題 
                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2.84655 1.74029C0.925074 1.2852 0.950356 2.64625 1.20318 3.38365L5.12195 7.68167L1.20318 12.0429C0.343585 13.9138 1.94059 14.0023 2.84655 13.8127L7.77663 8.75618C8.38341 7.9977 8.02945 7.2603 7.77663 6.9864L2.84655 1.74029Z" fill="#033159" stroke="black"/>
                 <path d="M8.73186 1.20675C7.01265 0.599971 6.83567 1.96523 6.96209 2.7237L12.3978 8.15943L6.96209 13.4688C6.60813 15.2385 7.99446 15.3017 8.73186 15.1121L14.6101 9.17073C15.6214 8.15943 15.0314 7.31668 14.6101 7.02172L8.73186 1.20675Z" fill="#033159" stroke="black"/>
 </svg>
 
-                    </router-link></button>
+</div></button>
 
 
                 <div class="next_quiz_frame"></div>
@@ -79,60 +75,78 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-
-
-
-
         </div>
 
     </section>
    
 </template>
+
+
+
+
+<script>
+
+function numberToChinese(num){
+    const chineseNums = ["一","二","三","四","五","六","七","八","九","十"];
+    return chineseNums[num - 1];
+}
+
+export default {
+    data(){
+        return{
+            questionNumber:1,
+            questions:[{
+                text:'請問哪一支電話號碼會是詐騙號碼？',
+                image: "../assets/img/p07_demo/p07_demoGame/quiz1.jpg",
+            },
+            {
+                text:"問題2問題2問題2問題2問題2問題2問題2",
+                image: "../assets/img/p07_demo/p07_demoGame/quiz1.jpg"
+            },
+            {
+                text:"問題3問題3問題3問題3問題3問題3問題3",
+                image: "../assets/img/p07_demo/p07_demoGame/quiz1.jpg"
+            }
+            ],
+
+
+          
+        };
+    },
+
+    computed:{
+        questionText(){
+            return this.questions[this.questionNumber -1 ].text;
+        },
+
+        questionImage(){
+            return this.questions[this.questionNumber - 1].image;
+        },
+    },
+
+
+    methods:{
+        changeQuestion(){
+            this.questionNumber +=1;
+        },
+        scrollToTop() {
+        window.scrollTo(0, 0);
+        },
+        changeQuestionAndScrollToTop() {
+        this.changeQuestion();
+        this.scrollToTop();
+  },
+        numberToChinese,
+    },
+
+
+
+
+
+
+
+};
+
+
+
+</script>
