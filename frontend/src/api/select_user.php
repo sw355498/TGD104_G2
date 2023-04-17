3b180aee47fc0ba32e5cd0cb49fe765d3fed3ccb
sw355498 <sw355498@gmail.com>
2023-04-17 16:04:16 +0800

    接收前端的request更改後端的where

--- frontend/src/api/select_user.php
+++ frontend/src/api/select_user.php
@@ -1,24 +1,32 @@
 <?php
-        //資料庫連線
-        require_once ("getConn_nopush.php");
+    //資料庫連線
+    require_once ("getConn_nopush.php");
 
+    //取得 HTTP POST  Client 端所傳送過來的原始資料
+    $postData = file_get_contents("php://input");
+    //將 JSON 格式的資料轉換成 PHP 陣列或物件形式的函式
+    $data = json_decode($postData, true);
+    //從 $data 中取得 whereVariable 變數的值
+    $whereVariable = $data['whereVariable'];
 
-        //建立SQL語法
-        $sql = "SELECT u.ID, u.ACCOUNT as '帳號', u.NICKNAME as '暱稱', u.CREATE_TIME as '建立日期', us.USER_STATUS_NAME as '登入方式', l.LOGIN_TYPE as '狀態'
-            FROM USER as u
-                join USER_STATUS as us
-                    on u.USER_STATUS_ID = us.ID
-                        join LOGIN_TYPE as l
-                            on u.LOGIN_TYPE_ID = l.ID
-                                WHERE u.ACCOUNT_TYPE_ID = 1;";
+    //建立SQL語法
+    $sql = "SELECT u.ID, u.ACCOUNT as '帳號', u.NICKNAME as '暱稱', u.CREATE_TIME as '建立日期', us.USER_STATUS_NAME as '狀態', l.LOGIN_TYPE as '登入方式'
+        FROM USER as u
+            join USER_STATUS as us
+                on u.USER_STATUS_ID = us.ID
+                    join LOGIN_TYPE as l
+                        on u.LOGIN_TYPE_ID = l.ID
+                            WHERE u.ACCOUNT_TYPE_ID = :whereVariable;";
 
-        //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
-        $statement = $pdo->query($sql);
+    //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(':whereVariable', $whereVariable);
+    $statement->execute();
 
-        //抓出全部且依照順序封裝成一個二維陣列
-        //PDO::FETCH_ASSOC 回傳一個index值是column name 的陣列
-        $data = $statement->fetchAll(PDO::FETCH_ASSOC);
-        //將資料型別轉換成陣列
-        $result = array('data' => $data);
-        echo json_encode($data);
+    //抓出全部且依照順序封裝成一個二維陣列
+    //PDO::FETCH_ASSOC 回傳一個index值是column name 的陣列
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+    //將資料型別轉換成陣列
+    $result = array('data' => $data);
+    echo json_encode($result);
 ?>
