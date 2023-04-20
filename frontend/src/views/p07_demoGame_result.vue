c81c2d4d596d66e974eca91ab47021f9cbaa1b8b
candybomb <shigotolucky@gmail.com>
2023-04-20 15:20:21 +0800

    增加會員彈窗

--- frontend/src/views/p07_demoGame_result.vue
+++ frontend/src/views/p07_demoGame_result.vue
@@ -6,19 +6,17 @@
 
             <!-- 彈跳視窗 -->
 
-            <div class="p07game_login_alert" v-if="showLoginAlert">
-                <div class="alert_cross" @click="closeLoginAlert">
+            <div class="p07game_login_alert" v-if="showLoginAlert && !isLogin">
+                <div class="alert_cross"  @click="closeLoginAlert"  >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>    
                 </div>   
                 <div class="login_panel">
                 
                 <p>您尚未登入會員，請問是否要登入會員記錄分數？</p>
                 
-                <button class="gamebutton game_login" >
-                <router-link to='/p08_userLogin'>    
-                    登入會員
-                </router-link>    
-                </button>
+                <button type="button" @click="showModal" class="gamebutton game_login">登入/註冊</button>
+
+    <Modal v-show="isModalVisible" @close="closeModal" />
                 <button class="gamebutton game_cancel" @click="closeLoginAlert">取消</button>    
                     
                 </div>                
@@ -96,6 +94,9 @@
         </div>
 
     </section>
+
+    <!-- footer -->
+    <frontFooter />
    
 </template>
 
@@ -103,14 +104,28 @@
 
 <script>
 
+import Modal from "@/components/userLogin.vue";
+import frontFooter from "@/components/f_footer.vue";
+
 
 export default {
+
+    name: "App",
+    components: {
+    Modal,
+    frontFooter,
+    },
+
+
 
     data(){
       return{
         texts:["測驗結果，你的防詐指數...."],
         score:null,
         showScore: false,
+        showLoginAlert: true, 
+        isLogin:false,
+        isModalVisible: false,
       }
     },
 
@@ -199,7 +214,27 @@ export default {
   
 
 
-  methods:{  //亂碼效果
+  methods:{  
+    
+    closeLoginAlert(){
+        this.showLoginAlert = false;
+        document.querySelector('.p07game_login_alert').classList.add('p07game_login_alert_close');
+    },
+
+    // goToLoginPage(){
+    //     this.isLogin = true;
+    //     this.$router.push('/p08_userLogin');
+    // },
+
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },    
+    
+        
+    //亂碼效果
         randomChar(){
         const lower = "abcdefghijklmnopqrstuvwxyz";
         const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -256,3 +291,11 @@ export default {
 
 
 </script>
+
+<style>
+
+.modal-wrapper .modal-container{
+    top:-100%;
+}
+
+</style>
