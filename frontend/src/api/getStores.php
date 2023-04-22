83f59e0057957229db2f7dee9519835b3e2b2ad7
itshsinyu <itshsinyu@gmail.com>
2023-04-23 00:30:35 +0800

    p07購物車抓超商地址

--- frontend/src/api/getStores.php
+++ frontend/src/api/getStores.php
@@ -3,11 +3,11 @@
     require_once("./getConn_nopush.php");
     $data = [];
     //建立SQL語法
-    $sql = "select ADDRESS from STORE where TYPE like :TYPE ";
+    $sql = "select * from STORE where TYPE like :TYPE ";
     
     $statement = $pdo->prepare($sql);
     $statement-> bindValue(':TYPE', '統一超商股份有限公司');
-    $statement-> bindValue(':TYPE', '全家便利商店股份有限公司');
+    // $statement-> bindValue(':TYPE', '全家便利商店股份有限公司');
     $statement-> execute();
     // PDO::FETCH_ASSOC 回傳一個index值是column name 的陣列
     $data = $statement->fetchAll(PDO::FETCH_ASSOC);
