8a01742dd33f133e1883f2691ce546618ce8ef71
KarenChuan <lanyonyon@gmail.com>
2023-04-23 13:26:00 +0800

    1.更新音檔 2.引用audio

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,105 +1,107 @@
-const { defineConfig } = require('@vue/cli-service')
-const webpack = require("webpack")
-const path = require('path')
-const { CKEditorTranslationsPlugin } = require( '@ckeditor/ckeditor5-dev-translations' );
-const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
-const { proxy } = require('jquery');
+const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
+const path = require("path");
+const {
+  CKEditorTranslationsPlugin,
+} = require("@ckeditor/ckeditor5-dev-translations");
+const { styles } = require("@ckeditor/ckeditor5-dev-utils");
+const { proxy } = require("jquery");
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const { createProxyMiddleware } = require('http-proxy-middleware');
 // const httpProxy = require('http-proxy');
 // const proxy = httpProxy.createProxyServer();
 
 module.exports = defineConfig({
-    transpileDependencies: [/ckeditor5-[^/\\]+[/\\]src[/\\].+\.js$/],
-    // publicPath:'./',
+  transpileDependencies: [/ckeditor5-[^/\\]+[/\\]src[/\\].+\.js$/],
+  // publicPath:'./',
 
-    // 'production-sub-path': process.env.NODE_ENV === 'production'
-    // ? '/tgd104/g2/dist/'
-    // : '/',
+  // 'production-sub-path': process.env.NODE_ENV === 'production'
+  // ? '/tgd104/g2/dist/'
+  // : '/',
 
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/tgd104/g2/dist/'
-    : '/',
-    configureWebpack: {
-        plugins: [
-            //jQuery 設定
-            new webpack.ProvidePlugin({
-                $:"jquery",
-                jQuery:"jquery", "windows.jQuery":"jquery"
-            }),
-            // new MiniCssExtractPlugin()
-        ]
-    },
-    chainWebpack: config => {
-        // 先刪除預設的svg配置
-        config.module.rules.delete("svg")
-      
-        // 新增 svg-sprite-loader 設定
-        config.module
-            .rule("svg-sprite-loader") 
-            .test(/\.svg$/)
-            .include
-            .add(path.resolve(__dirname, "src/assets/icon"))
-            .end()
-            .use("svg-sprite-loader")
-            .loader("svg-sprite-loader")
-            .options({ symbolId: "[name]" })
-            
-        // 修改 images-loader 配置
-        config.module
-            .rule("images")
-            .exclude.add(path.resolve(__dirname, "src/assets/icon"))
+  publicPath: process.env.NODE_ENV === "production" ? "/tgd104/g2/dist/" : "/",
+  configureWebpack: {
+    plugins: [
+      //jQuery 設定
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "windows.jQuery": "jquery",
+      }),
+      // new MiniCssExtractPlugin()
+    ],
+  },
+  chainWebpack: (config) => {
+    // 先刪除預設的svg配置
+    config.module.rules.delete("svg");
 
+    // 新增 svg-sprite-loader 設定
+    config.module
+      .rule("svg-sprite-loader")
+      .test(/\.svg$/)
+      .include.add(path.resolve(__dirname, "src/assets/icon"))
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({ symbolId: "[name]" });
 
-        // CKEditor設定
-        config.module
-        .rule('ckeditor')
-        .test(/\.ckeditor5\/[^/\\]+\/theme\/icons\/[^/\\]+\.svg$/)
-        .use('raw-loader')
-        .loader('raw-loader')
-        .end();
+    // 修改 images-loader 配置
+    config.module
+      .rule("images")
+      .exclude.add(path.resolve(__dirname, "src/assets/icon"));
 
-        config.module
-        .rule('vue')
-        .use('vue-loader')
-        .loader('vue-loader')
-        .tap(options => {
-          options.compilerOptions = {
-            ...options.compilerOptions,
-            isCustomElement: tag => tag.startsWith('ckeditor-')
-          };
-          return options;
-        })
-        .end();
+    // CKEditor設定
+    config.module
+      .rule("ckeditor")
+      .test(/\.ckeditor5\/[^/\\]+\/theme\/icons\/[^/\\]+\.svg$/)
+      .use("raw-loader")
+      .loader("raw-loader")
+      .end();
 
-        config.module
-        .rule( 'cke-css' )
-        .test( /ckeditor5-[^/\\]+[/\\].+\.css$/ )
-        .use( 'postcss-loader' )
-        .loader( 'postcss-loader' )
-        .tap( () => {
-            return {
-                postcssOptions: styles.getPostCssConfig( {
-                    themeImporter: {
-                        themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' ),
-                    },
-                    minify: true
-                } )
-            };
-        } );
-    },
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap((options) => {
+        options.compilerOptions = {
+          ...options.compilerOptions,
+          isCustomElement: (tag) => tag.startsWith("ckeditor-"),
+        };
+        options.transformAssetUrls = {
+          audio: "src",
+        };
+        return options;
+      })
+      .end();
 
-    // 串api需要的
-    devServer: {
-        proxy: {
-          '/madeByNeil': {
-            target: 'https://od.moi.gov.tw',
-            changeOrigin: true,
-            pathRewrite: {
-              '^/madeByNeil': ''
+    config.module
+      .rule("cke-css")
+      .test(/ckeditor5-[^/\\]+[/\\].+\.css$/)
+      .use("postcss-loader")
+      .loader("postcss-loader")
+      .tap(() => {
+        return {
+          postcssOptions: styles.getPostCssConfig({
+            themeImporter: {
+              themePath: require.resolve("@ckeditor/ckeditor5-theme-lark"),
             },
-            ws: true
-          }
-        }
-      }
-})
+            minify: true,
+          }),
+        };
+      });
+  },
+
+  // 串api需要的
+  devServer: {
+    proxy: {
+      "/madeByNeil": {
+        target: "https://od.moi.gov.tw",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/madeByNeil": "",
+        },
+        ws: true,
+      },
+    },
+  },
+});
