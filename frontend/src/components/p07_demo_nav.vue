2c010fbcad3f3fa2eac35f1b52e0d4e113441e2a
itshsinyu <itshsinyu@gmail.com>
2023-04-23 21:58:24 +0800

    改demo nav router

--- frontend/src/components/p07_demo_nav.vue
+++ frontend/src/components/p07_demo_nav.vue
@@ -3,10 +3,10 @@
         <h3>DEMO體驗</h3>
         <br>
         <div class="demoBtn p07_demoNav">
-            <router-link to="/p07demoShopping"><button class="medium_button">詐騙網站</button></router-link>
-            <router-link to="/p07demoGame"><button class="medium_button">詐騙知識測驗</button></router-link>
-            <router-link to="/p07demoPhone"><button class="medium_button">電話詐騙</button></router-link>
-            <router-link to="/p07demoLine"><button class="medium_button">愛情交友詐騙</button></router-link>            
+            <router-link to="/demoShopping"><button class="medium_button">詐騙網站</button></router-link>
+            <router-link to="/demoGame"><button class="medium_button">詐騙知識測驗</button></router-link>
+            <router-link to="/demoPhone"><button class="medium_button">電話詐騙</button></router-link>
+            <router-link to="/demoLine"><button class="medium_button">愛情交友詐騙</button></router-link>            
         </div>
     </div>
     <router-view></router-view>
