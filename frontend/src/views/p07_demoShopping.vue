74c0459ecb8f497b8391641706f909719e46d2d0
itshsinyu <itshsinyu@gmail.com>
2023-04-27 17:27:33 +0800

    購物網站機器人多加導覽按鈕拿掉掛元件的方式

--- frontend/src/views/p07_demoShopping.vue
+++ frontend/src/views/p07_demoShopping.vue
@@ -332,7 +332,18 @@
                     <div class="line"></div>
                 </div>
             </div>
-            <p07_demo_robot/>
+            <div class="demoRobot_container">
+                <p class="demoRobot_message">
+                    此購物網站為 DEMO 體驗，本站不會儲存您的個資！
+                    <button class="small_button" @click="showPopup = true">
+                        詐騙網站導覽
+                    </button>
+                </p>
+                <svg width="110" height="90px" style="">
+                    <use xlink:href="#robot" width="110" />
+                </svg>
+            </div>
+            <!-- <p07_demo_robot/> -->
         </div>
         <frontFooter />
     </main>
